ecc976c614b8244cf805b5f6f200ebcc96fe8c7f
ismael-miguel <espertalhao04@hotmail.com>
2016-08-13 23:28:29 +0100

    Update to works with the new leaderboard

--- tags/king-of-the-hill.js
+++ tags/king-of-the-hill.js
@@ -1,108 +1,92 @@
-var renderAnswers = function(){
-	
-	var last_score = 0;
-	var classification = 1;
-	var invalid_index = 0;
-	
-	$table.html('<thead>' +
-		'<tr>' +
-			'<th><i class="fa fa-hashtag" aria-hidden="true"></i></th>' +
-			'<th>Author</th>' +
-			'<th>Bot</th>' +
-			'<th>Votes</th>' +
-			'<th>&nbsp;</th>' +
-		'</tr>' +
-	'</thead>' +
-	'<tbody>' +
-		answers
-			.sort(function(a, b){
-				if(a.score != b.score)
-				{
-					return b.score - a.score;
-				}
-				return a.creation_date - b.creation_date;
-			})
-			.map(function(answer, index){
-				
-				if(index && last_score != answer.score)
-				{
-					classification = index + 1;
-				}
-				last_score = answer.score;
-				
-				return '<tr ' + (answer.is_accepted ? 'style="background:#e1ffdd"': '') + ' data-answer_id="' + answer.answer_id + '">' +
-					'<td data-order="' + (answer._meta.valid ? index + 1 : answers.length - (invalid_index++)) + '">' +
-						(
-							(
-								HEURISTIC_INVALID_MARK && answer._meta.HEURISTIC_INVALID && answer._meta.HEURISTIC_INVALID_SCORE < -2
-									? answer._meta.HEURISTIC_INVALID.map(function(comment){
-										return '<a href="' + comment.link + '" target="_blank"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>'
-									})
-									: ''
-							) + ' ' + classification
-						) +
-					'</td>' +
-					'<td>' +
-						'<a href="' + answer.owner.link + '" target="_blank">' +
-							answer.owner.display_name +
-						'</a>' +
-					'</td>' +
-					'<td>' +
-						answer._meta.bot +
-					'</td>' +
-					'<td data-order="' + (index + 1) + '">' +
-						answer.score +
-					'</td>' +
-					'<td>' +
-						'<a href="' + answer.link + '" target="_blank">View</a>' +
-					'</td>' +
-				'</tr>';
-			}).join('') +
-	'</tbody>');
-	
-	var $datatable = $table.DataTable({
-		order: [],
+{
+	header: [
+		'<i class="fa fa-hashtag" aria-hidden="true"></i>',
+		'Author',
+		'Bot',
+		'Votes',
+		'&nbsp;'
+	],
+	datatables: {
 		columnDefs: [
 			{orderable: false, targets: 4},
 			{searchable: false, targets: [0, 3, 4]},
 			{type: 'html-num', targets: 0}
 		]
-	});
-}
-
-var handleData = function(data){
-	if(!data.length)
-	{
-		return;
-	}
-	var $span = $('<span>');
-	var answers = [];
-	
-	data.map(function(answer){
-		$span.html(answer.body);
-		var $h1 = $span.find(':first-child').eq(0);
-			$h1.find('a').attr('target', '_blank');
-			
-		answer._meta = {
-			bot: $h1.html(),
-			bot_text: $h1.text().toLowerCase()
-		};
-		
-		return answer;
-	})
-	.forEach(function(answer){
-		
-		answers[answers.length] = answer;
-		
-		if(HEURISTIC_INVALID_MARK && answer.comments && answer.comments.length)
+	},
+	filters: false,
+	handleData: function(data){
+		if(!data.length)
 		{
-			analizeHeuristicValidity(answer);
+			return;
 		}
-	});
-	
-	setTimeout((function(){
-		renderAnswers(this);
-	}).bind(answers), 10);
-};
-
-handleData(answers);
+		var $span = $('<span>');
+		
+		return data.map(function(answer){
+			$span.html(answer.body);
+			var $h1 = $span.find(':first-child').eq(0);
+				$h1.find('a').attr('target', '_blank');
+				
+			answer._meta = {
+				bot: $h1.html(),
+				bot_text: $h1.text().toLowerCase()
+			};
+			
+			return answer;
+		});
+	},
+	sort: function(a, b){
+		if(a.score != b.score)
+		{
+			return b.score - a.score;
+		}
+		return b.creation_date - a.creation_date;
+	},
+	html: function(answer, index, meta){
+		if(!index)
+		{
+			meta.classification = 1;
+		}
+		
+		if(index && meta.last_score != answer.score)
+		{
+			meta.classification = index + 1;
+		}
+		meta.last_score = answer.score;
+		
+		return '<tr ' +
+				(answer.is_accepted ? 'style="background:#e1ffdd"': '') +
+				' data-answer_id="' + answer.answer_id + '" ' +
+				(
+					HEURISTIC_INVALID_MARK
+						? 'data-heuristic-invalid-score="' + answer._meta.HEURISTIC_INVALID_SCORE + '"'
+						: ''
+				) +
+			'>' +
+			'<td data-order="' + (index + 1) + '">' +
+				(
+					(
+						HEURISTIC_INVALID_MARK && answer._meta.HEURISTIC_INVALID && answer._meta.HEURISTIC_INVALID_SCORE < -2
+							? answer._meta.HEURISTIC_INVALID.map(function(comment){
+								return '<a href="' + comment.link + '" target="_blank"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></a>'
+							})
+							: ''
+					) + ' ' + meta.classification
+				) +
+			'</td>' +
+			'<td>' +
+				'<a href="' + answer.owner.link + '" target="_blank">' +
+					answer.owner.display_name +
+				'</a>' +
+			'</td>' +
+			'<td>' +
+				answer._meta.bot +
+			'</td>' +
+			'<td data-order="' + (index + 1) + '">' +
+				answer.score  +
+			'</td>' +
+			'<td>' +
+				'<a href="' + answer.link + '" target="_blank">View</a>' +
+			'</td>' +
+		'</tr>';
+	}
+}
